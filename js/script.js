1cafcaa2abb20eb977ad7478756c6328af90e052
MatteoAngelini <matteoangelini45@gmail.com>
2025-03-12 17:20:41 +0100

    commenti js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 //Prova collegamento//
-/* alert("Ciao")
- */
+/* alert("Ciao")*/
+
+/* Dati:
+
+- Ore:10 ore
+
+- Codici sconto: YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24.
+
+- Sconto: 25%
+
+Il prezzo orario per una commissione varia in questo modo:
+
+- se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.50€/l’ora
+
+- se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.30€/l’ora
+
+- se la commissione riguarda l’analisi progettuale il prezzo orario è di 33.60€/l’ora
+
+- Risultato del prezzo finale deve essere in forma umana con 2 decimali e il simbolo dell'euro  */
+
+
+
+
+
+
+
+
+/* Richiesta dati:
+
+- Prelevare dati di input dopo click del bottone
+
+- Inserire oggetti all'interno di un array che viene creato ogni volta che viene cliccato il tasto */
+
+
