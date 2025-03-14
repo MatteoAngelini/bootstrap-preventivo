7dbd71bb011ccbf92d6907ee1bd61a2ab2e8ba6a
MatteoAngelini <matteoangelini45@gmail.com>
2025-03-14 11:42:28 +0100

    etichette codici promozionali al 60%

--- js/script.js
+++ js/script.js
@@ -37,11 +37,13 @@ const inputProject = document.getElementById("project")
 const inputMessaggio = document.getElementById("messaggio");
 const inputCodice = document.getElementById("codice")
 const prezzoFinale = document.getElementById("prezzo-finale");
+const outputSconto = document.getElementById("sconto")
 
 
 
 /* Event Listner */
 preventivoForm.addEventListener("submit", gestisciForm,);
+
 
 /* Funzioni */
 function gestisciForm(event) {
@@ -67,15 +69,15 @@ function gestisciForm(event) {
     if (lavoro === backend) {
         const prezzo = 20.50 * 10; 
         prezzoDecimali = prezzo.toFixed(2)
-        prezzoFinale.innerHTML = `<div class="col-lg-6 col-sm-12 col-md-6 mt-3"><span id="prezzo-finale" class="text-center align-middle mt"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark">${prezzoDecimali}</p></span></div>`
+        prezzoFinale.innerHTML = `<span id="prezzo-finale" class="text-center align-middle"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimali}</p></span>`
     } else if (lavoro === frontend) {
         const prezzo = 15.30 * 10;
         prezzoDecimali = prezzo.toFixed(2)
-        prezzoFinale.innerHTML = `<div class="col-lg-6 col-sm-12 col-md-6 mt-3"><span id="prezzo-finale" class="text-center align-middle mt"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark">${prezzoDecimali}</p></span></div>`
+        prezzoFinale.innerHTML = `<span class="text-center align-middle"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimali}</p></span>`
     } else if (lavoro === project) {
         const prezzo = 33.60 * 10;
         prezzoDecimali = prezzo.toFixed(2)
-        prezzoFinale.innerHTML = `<div class="col-lg-6 col-sm-12 col-md-6 mt-3"><span id="prezzo-finale" class="text-center align-middle mt"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark">${prezzoDecimali}</p></span></div>`
+        prezzoFinale.innerHTML = `<span class="text-center align-middle"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimali}</p></span>`
     } else {
         
     }
@@ -84,29 +86,27 @@ function gestisciForm(event) {
     
 
     /* Array Codici sconto e ciclo for*/
-     const codici = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24",]
-     const codiceVuoto = [""]   
+     const codici = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24",]  
+     const codiceNum = parseInt(codice) 
     
-
+    
      let prezzoDecimaliScontato;
      codici.forEach(function (curSconto, i){
         let codiceSconto = curSconto;
         if (codice === codiceSconto) {
-        prezzoScontato = prezzoDecimali - (prezzoDecimali * 25 / 100);
-        prezzoDecimaliScontato = prezzoScontato.toFixed(2)
-        prezzoFinale.innerHTML = `<div class="col-lg-6 col-sm-12 col-md-6 mt-3"><span id="prezzo-finale" class="text-center align-middle mt"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark">${prezzoDecimaliScontato}</p></span></div><div class="col-lg-6 col-sm-12 col-md-6 mt-3"><button type="button" class="btn btn-sm btn-success">Sconto del 25 % applicato</button></div>`
-        } else if(codice === codiceVuoto){
-        prezzoFinale.innerHTML = `<div class="col-lg-6 col-sm-12 col-md-6 mt-3"><span id="prezzo-finale" class="text-center align-middle mt"><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark">${prezzoDecimali}</p></span></div><div class="col-lg-6 col-sm-12 col-md-6 mt-3"><button type="button" class="btn btn-sm btn-warning">Nessun codice sconto applicato</button></div>`
-        } else{
-      
+            prezzoScontato = prezzoDecimali - (prezzoDecimali * 25 / 100);
+            prezzoDecimaliScontato = prezzoScontato.toFixed(2)
+            prezzoFinale.innerHTML = `<span class="text-center align-middle "><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimaliScontato}</p></span>`
+            sconto.innerHTML = `<button type="button" class="btn btn-sm btn-success" id="sconto-applicato">Sconto del 25 % applicato</button>`
+        } else if(codice === ""){
+            prezzoFinale.innerHTML = `<span class="text-center align-middle "><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimali}</p></span>`
+        } else {
+            prezzoFinale.innerHTML = `<span class="text-center align-middle "><h5><strong>Prezzo finale</strong></h5><p><i>&euro;</i><span class="fw-bold fs-5 text-dark"> ${prezzoDecimali}</p></span>`
+            sconto.innerHTML = `<button type="button" class="btn btn-sm btn-danger" id="sconto-applicato">Codice non valido</button>`
         }
      });
-     
-     
 
     
-
-
     /* Ripulisco gli input */
     preventivoForm.reset();
 
